17b455d75cb2d706ce326536edc129d2a873c7b0
Canis Ignem <jonperezetxebarria@gmail.com>
2020-11-14 17:26:23 +0100

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import tensorflow.compat.v1 as tf
-    tf.disable_v2_behavior()    
+import tensorflow as tf   
 from IPython.display import display
 from IPython.display import Image
 
