83748740aed2e85c327701683c05c3dbd489d632
Canis Ignem <jonperezetxebarria@gmail.com>
2020-11-14 17:27:41 +0100

    Update nn.py

--- nn.py
+++ nn.py
@@ -5,56 +5,53 @@ from IPython.display import display
 from IPython.display import Image
 
 
-def double_linear_model(data):
+def MLP(data, classes, test_data, test_classes):
     tf.reset_default_graph()
-
-    x1 = tf.placeholder("float", name="x1")
-    x2 = tf.placeholder("float", name="x2")
-    y = tf.placeholder("float", name="y")
-
-    w11 = tf.Variable(0.0, name="weight1_model1")
-    w21 = tf.Variable(0.0, name="weight2_model1")
-    b1 = tf.Variable(0.0, name="bias_model1")
     
-    w12 = tf.Variable(0.0, name="weight1_model2")
-    w22 = tf.Variable(0.0, name="weight2_model2")
-    b2 = tf.Variable(0.0, name="bias_model2")
-
-    model1 = tf.sigmoid(tf.add(tf.add(tf.multiply(x1, w11), tf.multiply(x2, w21)), b1))
-    model2 = tf.sigmoid(tf.add(tf.add(tf.multiply(x1, w12), tf.multiply(x2, w22)), b2))
+    # One placeholder for all features (as a matrix) and another for the prediction
+    x = tf.placeholder(tf.float32, [None, 784])
+    y = tf.placeholder(tf.float32, [None, 10])
     
-    combined = tf.multiply(model1, 2.) + model2
+    # First layer: one matrix of weights (on top of the known features) and a vector of biases
+    w1 = tf.Variable(tf.zeros([784, 100]), name="layer1_weights")
+    b1 = tf.Variable(tf.zeros([100]), name="layer1_biases")
+    
+    # Second layer: one matrix of weights (on top of the abstract features) and a vector of biases
+    w2 = tf.Variable(tf.zeros([100, 10]), name="layer2_weights")
+    b2 = tf.Variable(tf.zeros([10]), name="layer2_biases")
+    
+    linear_model = tf.matmul(x, w1) + b1
+    # First layer and probability prediction
+    layer1 = tf.sigmoid(linear_model)
+    
+    linear_model2 = tf.matmul(layer1,w2) + b2
+    prediction = tf.add(tf.matmul(layer1,w2),b2)
 
-    loss = tf.losses.mean_squared_error(y, combined)
+    # Loss and class prediction
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=prediction)
+    class_pred = tf.argmax(prediction, axis =1)
 
+    # Optimizer & initialization
     optimizer = tf.train.AdamOptimizer(learning_rate=.001).minimize(loss)
-
     init = tf.global_variables_initializer()
 
-    training_epochs = 40000
+    training_epochs = 10000
     train_n_samples = data.shape[0]
-    display_step = 10000
+    display_step = 200
 
     mini_batch_size = 100
     n_batch = train_n_samples // mini_batch_size + (train_n_samples % mini_batch_size != 0)
     
-    for i in range(data.shape[0]):
-        plt.plot(data[i,0], data[i,1],'o', c=cmap[np.int(data[i,2])])
     with tf.Session() as sess:
         sess.run(init)
         for epoch in range(training_epochs):
             i_batch = (epoch % n_batch)*mini_batch_size
-            batch = data[i_batch:i_batch+mini_batch_size, 0], data[i_batch:i_batch+mini_batch_size, 1], data[i_batch:i_batch+mini_batch_size, 2]
-            sess.run(optimizer, feed_dict={x1: batch[0], x2: batch[1], y: batch[2]})
+            batch = data[i_batch:i_batch+mini_batch_size,:], classes[i_batch:i_batch+mini_batch_size, :]
+            sess.run(optimizer, feed_dict={x: batch[0], y: batch[1]})
             if (epoch+1) % display_step == 0:
-                malda1, c1 = -sess.run(w11)/sess.run(w21), -sess.run(b1)/sess.run(w21)
-                malda2, c2 = -sess.run(w12)/sess.run(w22), -sess.run(b2)/sess.run(w22)
-                plt.plot([0, 4], [c1,c1+malda1*4], label="Model1, Epoch " + str(epoch+1))
-                plt.plot([0, 4], [c2,c2+malda2*4], label="Model2, Epoch " + str(epoch+1))
-                cost = sess.run(loss, feed_dict={x1: data[:, 0], x2: data[:, 1], y: data[:, 2]})
-                print("Epoch:", str(epoch+1), "Error:", np.mean(cost))
-        params = [sess.run(w11), sess.run(w21), sess.run(b1), sess.run(w12), sess.run(w22), sess.run(b2)]
-    plt.legend()
-    plt.ylim((-3.2,3.2))
-    plt.xlim((-0.2,4.2))
-    return params
+                cost = sess.run(loss, feed_dict={x: batch[0], y: batch[1]})
+                acc = np.sum(np.argmax(test_classes, axis=1) == sess.run(class_pred, feed_dict={x: test_data}))/test_classes.shape[0]
+                print("Epoch:", str(epoch+1), "Error:", np.mean(cost), "Accuracy:", acc)
+        parameters = sess.run([w1, b1, w2, b2])
+        test_predictions = sess.run(class_pred, feed_dict={x: test_data})
+    return parameters, test_predictions
